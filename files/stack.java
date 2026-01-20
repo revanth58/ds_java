cb6f579f08d9e87fdd3ba9346722550305ead115
revanth58 <revanthk7777@gmail.com>
2026-01-20 14:29:09 +0530

    dfnkdf

---
+++ files/stack.java
@@ -0,0 +1,68 @@
+class Stack {
+    int arr[];
+    int top;
+
+    Stack(int size){
+        arr = new int[size];
+        top = -1;
+    }
+    public void push(int x){
+        if( top == arr.length-1){
+            System.out.print("ERROR : stack is already full\n");
+            return ;
+        }
+        top++;
+        arr[top] = x;
+        System.out.print("PUSH successful\n");
+    }
+
+    public void pop(){
+        if(top == -1){
+            System.out.print("ERROR : stack is already empty\n");
+            return ;
+        }
+        top = top-1;
+        System.out.print("POP successful\n");
+    }
+
+    public int peek(){
+        if(top == -1){
+            System.out.print("ERROR : stack is currently empty\n");
+            return ;
+        }
+        return arr[top];
+    }
+
+    public boolean isempty(){
+        if( top == -1)return true;
+        return false;
+    }
+
+    public int size(){
+        return top+1;
+    }
+    public void print_stack(){
+        for(int i=0;i<=top; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+}
+
+class S {
+    public static void main(String[] args) {
+        Stack s1 = new Stack(6);
+        s1.push(5);
+        s1.push(6);
+        s1.push(7);
+        s1.push(8);
+        s1.push(9);
+        s1.push(10);
+        s1.print_stack();
+        s1.push(11);
+        for(int i=0;i<6;i++ ){
+            s1.pop();
+        }
+        s1.pop();
+    }
+}
