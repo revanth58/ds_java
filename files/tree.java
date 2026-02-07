3d6991f08f4a721cba31c09af25f1b8995f67df7
revanth58 <revanthk7777@gmail.com>
2026-02-07 11:48:53 +0530

    sgfernkj

---
+++ files/tree.java
@@ -0,0 +1,80 @@
+class Node{
+    int value;
+    Node left;
+    Node right;
+
+    Node(int val){
+        value = val;
+        left = right = null;
+    }
+}
+
+class T{
+    public static void preorder(Node root){
+        if(root == null)return ;
+        System.out.print(root.value + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void inorder(Node root){
+        if(root == null)return ;
+        inorder(root.left);
+        System.out.print(root.value + " ");
+        inorder(root.right);
+    }
+
+    public static void postorder(Node root){
+        if(root == null)return ;
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.value + " ");
+    }
+
+    public static int height(Node root){
+        if(root == null) return 0;
+        int a= height(root.left);
+        int b = height(root.right);
+        if(a<b)a=b;
+        return 1+a;
+    }
+
+    public static boolean search (Node root, int value){
+        if(root == null) return false;
+        if(root.value == value) return true;
+
+        return search(root.left, value) || search(root.right, value);
+    }
+
+    public static void main(String[] args){
+        //Node n0 = new Node(0);
+        Node n1 = new Node(1);
+        Node n7 = new Node(7);
+        Node n20 = new Node(20);
+        Node n64 = new Node(64);
+        Node n77 = new Node(77);
+        Node n88 = new Node(88);
+        Node n101 = new Node(101);
+        Node n200 = new Node(200);
+        /*Node n9 = new Node(9);
+        Node n10 = new Node(10);
+        Node n11 = new Node(11);*/
+        
+        n77.left = n20;
+        n77.right = n101;
+        n20.left = n1; n20.right = n64;
+        n1.right = n7;
+        n101.left = n88;
+        n101.left = n200;
+        System.out.print("pre order : ");
+        preorder(n77);
+        System.out.print("\nin order : ");
+        inorder(n77);
+        System.out.print("\npost order : ");
+        postorder(n77);
+        System.out.print("height of tree : " + height(n77) + "\n");
+        System.out.print("searching for 64 : " + search(n77,64) + "\n");
+        System.out.print("searching for 64 : " + search(n77,64) + "\n");
+        
+    }
+}
